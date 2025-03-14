9b06cc85525fc1eac4e53046fb7f13fc1fa072be
ryschoi <choi.re@northeastern.edu>
2025-03-13 23:32:10 -0400

    version3 - compressed

--- src/Portfolio/About/about.tsx
+++ src/Portfolio/About/about.tsx
@@ -2,48 +2,53 @@ import "./about.css";
 export default function About() {
   return (
     <div id="about">
-      <img id="headshot" alt="Headshot" src="images/headshot.jpg" />
-
       <div id="text">
+        <div className="circular-container">
+          <img id="headshot" src="images/headshot-square.png" />
+        </div>
         <div className="bio">
           {/* tech as a tool, design as a principle, data as the content, and humans as the reason */}
-          <p id="greeting">Hi, I'm Rebecca!</p><br />
+          <h2 id="greeting">Hi, I'm Rebecca!</h2><br />
 
-          <p>
-            I'm currently a second-year student studying Computer Science
+          <p className="mid-text">
+            I'm a second-year student studying Computer Science
             and Design at Northeastern University in Boston.
-          </p><br />
+          </p>
 
-          {/* <p className="heading gray">As a designer & developer...</p> */}
-          <p>
-            I am passionate about understanding how people think and behave. In design / UX,
-            I have found a discipline where I can apply my insights and fuel my passion.
+          <p className="subheading gray">I am passionate about understanding how people think and behave.</p>
+          <p className="mid-text">
+            In design, I have found a discipline where I can apply my insights and fuel my passion.
             Whether it's in coding or visual design, I work with the human nature in mind,
             aiming to create quality products that new users can interact with in a familiar way.<br /><br />
 
-            I am experienced in UI/UX, design, web development, and data visualization, but
+            I have experience in UI/UX, design, web development, and data visualization, but
             looking to being a part of anything that combines tech, design, and data to
-            create something meaningful and useful for our world.<br /><br />
+            create something meaningful and useful for our world.
           </p>
 
-          <p className="heading gray">Right now I am...</p>
-          <ul>
-            <li>Learning to clean up, analyze, and visualize data in Python and R</li>
-            <li>Working as a designer on a team of student developers and designers to create a co-op review platform</li>
-            <li>Refining this website to store more of my projects and restructuring it to be more easily maintainable for the future. Next step: making it mobile-reponsive!</li>
-          </ul><br />
-
-          <p className="heading gray">Outside of work...</p>
+          <p className="subheading gray">Currently I am...</p>
           <p>
+            <ul className="mid-text">
+              <li className="mid-text">Learning to clean up, analyze, and visualize data in Python and R</li>
+              <li className="mid-text">Working as a designer at Sandbox Northeastern on a team of student developers and designers to create a co-op review platform</li>
+              <li className="mid-text">Refining this website to store more of my projects and restructuring it to be more easily maintainable for the future. Next step: making it mobile-reponsive!</li>
+            </ul>
+          </p>
+
+
+          <p className="subheading gray">Outside of work...</p>
+          <p className="mid-text">
             I love trees, playing the bass, long silent train rides, and kombucha.
-          </p><br />
+          </p>
+          <br /><br />
+          <hr />
+          <br /><br />
 
-          <p className="heading gray">Contact me</p>
-          <p>
-            <a className="underline" href="choi.re@northeastern.edu">choi.re@northeastern.edu</a><br />
+          <p className="mid-text">
+            <a className="mid-text underline" href="choi.re@northeastern.edu">choi.re@northeastern.edu</a><br />
             510-682-0020<br /><br />
 
-            <a className="underline" href="https://www.linkedin.com/in/ryschoi">LinkedIn</a> | <a className="underline" href="images/Rebecca_Choi_Resume.pdf">Resume</a>
+            <a className="mid-text underline" href="https://www.linkedin.com/in/ryschoi">LinkedIn</a> | <a className="underline mid-text" href="images/Rebecca_Choi_Resume.pdf">Resume</a>
           </p><br /><br />
         </div>
 
