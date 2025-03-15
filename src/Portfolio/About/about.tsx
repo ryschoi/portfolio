f676e78cfa12b273030503c36384881f14e4b0fc
ryschoi <choi.re@northeastern.edu>
2025-03-15 15:06:05 -0400

    complete project header

--- src/Portfolio/About/about.tsx
+++ src/Portfolio/About/about.tsx
@@ -2,62 +2,62 @@ import "./about.css";
 export default function About() {
   return (
     <div id="about">
-      <div id="text">
-        <div className="circular-container">
-          <img id="headshot" src="images/headshot-square.png" />
-        </div>
-        {/* tech as a tool, design as a principle, data as the content, and humans as the reason */}
-        <h2 id="greeting">Hi, I'm Rebecca!</h2>
+      <div className="circular-container">
+        <img id="headshot" src="images/headshot-square.png" />
+      </div>
 
-        <p className="subheading gray no-margin-top">
-          I'm a second-year student studying Computer Science
-          and Design at Northeastern University in Boston.
+      {/* tech as a tool, design as a principle, data as the content, and humans as the reason */}
+      <h2 id="greeting">Hi, I'm Rebecca!</h2>
+
+      <p className="subheading gray no-margin-top">
+        I'm a second-year student studying Computer Science
+        and Design at Northeastern University in Boston.
+      </p>
+
+      <div>
+        <p className="subheading gray">I am passionate about understanding how people think and behave.</p>
+        <p className="mid-text">
+          In design, I have found a discipline where I can apply my insights and fuel my passion.
+          Whether it's in coding or visual design, I work with the human nature in mind,
+          aiming to create quality products that new users can interact with in a familiar way.<br /><br />
+
+          I have experience in UI/UX, design, web development, and data visualization, but
+          looking to being a part of anything that combines tech, design, and data to
+          create something meaningful and useful for our world.
         </p>
+      </div>
 
-        <div>
-          <p className="subheading gray">I am passionate about understanding how people think and behave.</p>
-          <p className="mid-text">
-            In design, I have found a discipline where I can apply my insights and fuel my passion.
-            Whether it's in coding or visual design, I work with the human nature in mind,
-            aiming to create quality products that new users can interact with in a familiar way.<br /><br />
+      <div>
+        <p className="subheading gray">Currently I am...</p>
+        <p>
+          <ul className="mid-text">
+            <li className="mid-text">Learning to clean up, analyze, and visualize data in Python and R</li>
+            <li className="mid-text">Working as a designer at Sandbox Northeastern on a team of student developers and designers to create a co-op review platform</li>
+            <li className="mid-text">Refining this website to store more of my projects and restructuring it to be more easily maintainable for the future. Next step: making it mobile-reponsive!</li>
+          </ul>
+        </p>
+      </div>
 
-            I have experience in UI/UX, design, web development, and data visualization, but
-            looking to being a part of anything that combines tech, design, and data to
-            create something meaningful and useful for our world.
-          </p>
-        </div>
+      <div>
+        <p className="subheading gray">Outside of work...</p>
+        <p className="mid-text">
+          I love trees, playing the bass, long silent train rides, and kombucha.
+        </p>
+      </div>
 
-        <div>
-          <p className="subheading gray">Currently I am...</p>
-          <p>
-            <ul className="mid-text">
-              <li className="mid-text">Learning to clean up, analyze, and visualize data in Python and R</li>
-              <li className="mid-text">Working as a designer at Sandbox Northeastern on a team of student developers and designers to create a co-op review platform</li>
-              <li className="mid-text">Refining this website to store more of my projects and restructuring it to be more easily maintainable for the future. Next step: making it mobile-reponsive!</li>
-            </ul>
-          </p>
-        </div>
+      {/* <hr /> */}
+      <div>
+        <p className="subheading gray">Contact</p>
+        <p className="mid-text">
+          <a className="mid-text underline" href="choi.re@northeastern.edu">choi.re@northeastern.edu</a><br />
+          510-682-0020<br /><br />
 
-        <div>
-          <p className="subheading gray">Outside of work...</p>
-          <p className="mid-text">
-            I love trees, playing the bass, long silent train rides, and kombucha.
-          </p>
-        </div>
-
-        {/* <hr /> */}
-        <div>
-          <p className="subheading gray">Contact</p>
-          <p className="mid-text">
-            <a className="mid-text underline" href="choi.re@northeastern.edu">choi.re@northeastern.edu</a><br />
-            510-682-0020<br /><br />
-
-            <a className="mid-text underline" href="https://www.linkedin.com/in/ryschoi">LinkedIn</a> | <a className="underline mid-text" href="images/Rebecca_Choi_Resume.pdf">Resume</a>
-          </p>
-        </div>
+          <a className="mid-text underline" href="https://www.linkedin.com/in/ryschoi">LinkedIn</a> | <a className="underline mid-text" href="images/Rebecca_Choi_Resume.pdf">Resume</a>
+        </p>
+      </div>
 
 
-        {/* <div id="contact">
+      {/* <div id="contact">
           Contact
           <p id="contact-info">
             <a className="underline" href="choi.re@northeastern.edu">choi.re@northeastern.edu</a><br />
@@ -66,7 +66,6 @@ export default function About() {
             <a className="underline" href="https://www.linkedin.com/in/rebecca-choi-3479b916a">LinkedIn</a> | <a className="underline" href="images/Rebecca_Choi_Resume.pdf">Resume</a>
           </p>
         </div> */}
-      </div>
-    </div >
+    </div>
   );
 }
--- src/Portfolio/Work/Projects/projectHeader.tsx
+++ src/Portfolio/Work/Projects/projectHeader.tsx
@@ -2,29 +2,19 @@ import { Project } from "./Project";
 
 export default function ProjectHeader({ project }: { project: Project }) {
     return (
-        <div className="proj-two-thirds slim-fit">
-            <div className="col8">
-                <h2 className="project-heading-title">{project.name}</h2>
-                <p>{project.summary}</p>
+        <div className="col48 slim-fit">
+            <div className="col22-align-left">
+                <div className="col8">
+                    <h2 className="project-heading-title">{project.name}</h2>
+                    <p>{project.summary}</p>
+                </div>
+                <p className="project-heading-details gray">{project.year}<br />{project.tags}</p>
             </div>
-            <p className="project-heading-details gray">{project.year}<br />{project.tags}</p>
             <div className="row-space-between" id="proj-attributes">
-                <div>
-                    <p className="gray bold">PROJECT TYPE</p>
-                    <p>{project.type}</p>
-                </div>
-                <div>
-                    <p className="gray bold">TEAM & ROLE</p>
-                    <p>{project.role}</p>
-                </div>
-                <div>
-                    <p className="gray bold">MEDIUM</p>
-                    <p>{project.medium}</p>
-                </div>
-                <div>
-                    <p className="gray bold">TOOLS</p>
-                    <p>{project.tools}</p>
-                </div>
+                <div><p className="gray bold">PROJECT TYPE</p>{project.type}</div>
+                <div><p className="gray bold">TEAM & ROLE</p>{project.team}<br />{project.role}</div>
+                <div><p className="gray bold">MEDIUM</p>{project.medium}</div>
+                <div><p className="gray bold">TOOLS</p>{project.tools}</div>
             </div>
             <p className="description">{project.description}</p>
         </div>
