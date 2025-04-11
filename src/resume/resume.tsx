06105b53a7d0e0ff4217608213db4b5be891dbf3
ryschoi <choi.re@northeastern.edu>
2025-04-10 21:16:33 -0400

    fix resume background

--- src/resume/resume.tsx
+++ src/resume/resume.tsx
@@ -6,8 +6,8 @@ export default function Resume() {
     <div id="port-resume">
       <div className="col22">
         <a className="underline gray" href="images/Rebecca_Choi_Resume.pdf">View as pdf</a>
-        <div id="resume-img-cont">
-          <img className="resume-img-cont" id="resume-img" src="images/Rebecca_Choi_Resume.png" alt="" width="85%" />
+        <div className="resume-img-cont">
+          <img className="" id="resume-img" src="images/Rebecca_Choi_Resume.png" alt="" width="85%" />
         </div>
       </div>
     </div >
