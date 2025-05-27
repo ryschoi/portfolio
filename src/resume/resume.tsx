f2d4029109bc620456ac8faa4881e938cda33032
ryschoi <choi.re@northeastern.edu>
2025-05-26 18:15:47 -0700

    resume image bigger for mobile

--- src/resume/resume.tsx
+++ src/resume/resume.tsx
@@ -3,7 +3,7 @@ import "./resume.css";
 export default function Resume() {
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col gap-[1rem] w-[85vw] max-w-[50rem] align-center text-center">
+      <div className="flex flex-col gap-[1rem] w-[90vw] max-w-[50rem] align-center text-center">
         <a className="underline gray" href="images/Rebecca_Choi_Resume.pdf">View as pdf</a>
         <div className="resume-shadow p-[2rem]">
           <img className="w-full" src="images/Rebecca_Choi_Resume.png" alt="" />
