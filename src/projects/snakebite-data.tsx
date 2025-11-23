2c2d4a5eaaae0906fd187ac833440d6f5e032143
ryschoi <choi.re@northeastern.edu>
2025-11-22 22:21:40 -0800

    update snakebite data content

--- src/projects/snakebite-data.tsx
+++ src/projects/snakebite-data.tsx
@@ -18,32 +18,32 @@ export default function SnakebiteData() {
                     <SectionHeader
                         title="Part 1"
                         description="Data collection & engineering"
-                        optional="I started by downloading data sets for various climate conditions from Our World in Data. From there, I used Excel to clean up each dataset and combine them into one workbook that I could easily filter through and map. Even though each dataset covered global data by country, there were still variations in the data structure: some countries did not have recorded data in some datasets, and some datasets listed continents as countries, leading to duplicate values."/>
+                        optional="I started by downloading data sets for various climate conditions from Our World in Data. From there, I used Excel to clean up each dataset and combine them into one workbook that I could easily filter through and map. Even though each dataset covered global data by country, there were still variations in the data structure: some countries did not have recorded data in some datasets, and some datasets listed continents as countries, leading to duplicate values." />
                 </div>
 
                 {/* DATA VISUALIZATION */}
                 <div className="section" id="data-visualization">
-                    <SectionHeader title="Part 2" description="Data visualization" optional="I used Excel to create initial data visualizations like maps and charts. From there, I copied them over into Illustrator where I edited them more stylistically and pieced them together into the poster orientation." />
+                    <SectionHeader title="Part 2: Data visualization" description="Establishing a clear visual language and data story" optional="I used Excel to create initial data visualizations like maps and charts. From there, I copied them over into Illustrator where I edited them more stylistically and pieced them together into the poster orientation." />
 
                     {/* FIRST DRAFT */}
-                    <div className="flex flex-col w-full">
-                        <div className="img-bg rounded flex place-content-center w-full">
+                    <div className="flex flex-col  w-slim self-center">
+                        <div className="img-bg rounded flex place-content-center">
                             <div className="self-center my-[3vw] subtle-shadow flex flex-wrap gap-[4vw] place-content-center">
                                 <img src="/images/work/snakebite-data/s-data-crit1.png" alt="" className="w-[36vw] min-w-[332px]" />
-                                <img src="/images/work/snakebite-data/snakebite-data-feedback.png" alt="" className="w-[36vw] min-w-[332px]" />
+                                {/* <img src="/images/work/snakebite-data/snakebite-data-feedback.png" alt="" className="w-[36vw] min-w-[332px]" /> */}
                             </div>
                         </div>
                         <p className="caption cap-top">first iteration</p>
                     </div>
                     {/* COMMENTS */}
                     <div className="flex flex-col w-slim self-center">
-                        <p>Comments:</p>
+                        <p className="">Comments on the first iteration:</p>
                         <ul>
-                            <li>Box plot is too technical–would take too long to read while some may not even know how to read it</li>
-                            <li>Re-order items in two right visualizations to be in the same order</li>
-                            <li>Turn map into a choropleth to show more of a range of snakebite conditions, rather than a black and white more than or below average</li>
-                            <li>Make it look less like a two-column layout–establish a more clear flow</li>
-                            <li>Make the green less bright; stands out too much</li>
+                            <li>Box plot is too technical–would <span className="bold">take too long to read</span> while some may not even know how to read it</li>
+                            <li>Reorder items in the two visualizations on the right to be in the same order to <span className="bold">make it easier to move between the two charts</span></li>
+                            <li>Turn map into a choropleth to <span className="bold">capture the range</span> of snakebite conditions, rather than a black-and-white, more-than or below-average</li>
+                            <li>Make it less of a two-column layout to <span className="bold">establish a clearer flow through the posters</span></li>
+                            <li>Increase contrast of the green and the light gray</li>
                         </ul>
                     </div>
                 </div>
@@ -51,26 +51,98 @@ export default function SnakebiteData() {
                 {/* ISOTYPES */}
                 <div className="section align-center">
                     <div className="thing w-slim">
-                        <h3>Isotypes</h3>
-                        <p>At first, to highlight the difference in climate conditions, I had illustrations of what those conditions might look like (i.e. more jagged land or more rainfall). However, I found that other than the numbers under the illustrations, there was no way to tangibly grasp the difference in measurements.<br /><br />For the final version, I turned the numbers into isotypes with circles representing the actual proportions of the numbers. This way, it is a lot easier to tell the values of each measurement, and just how much the differences are.</p>
+                        <div className="flex flex-col">
+                            <h3>Isotypes</h3>
+                            <h3 className="gray">Representing data in a visual and proportionately-accurate way</h3>
+                        </div>
+                        <p>For the group of visualizations comparing climate conditions in countries with high rates of snakebite and the rest of the world, I went through three different variations of representing the values.</p>
                     </div>
                     {/* IMAGES */}
-                    <div className="work-img-group-col-bigger">
-                        <div>
-                            <div className="s-data-bg work-add-img-cont">
-                                <div className="self-center m-[1rem]">
-                                    <img src="/images/work/snakebite-data/isotype2.png" alt="" className="w-full min-w-[18rem]" />
+                    {/* <div className="gray-narrow-container rounded self-center">
+                        <div className="flex flex-col mob-mid-w self-center gap-[3rem]">
+                            <div className="flex gap-[1.5rem]">
+                                <div className="flex flex-col gap-[0.4rem] w-1/3">
+                                    <p className="caption">first version: bars</p>
+                                    <p className="">Arbitrary bar sizes, only takeaway is the difference in size and the actual value difference</p>
+                                </div>
+                                <div className="s-data-bg work-add-img-cont subtle-shadow w-2/3">
+                                    <div className="self-center m-[1rem]">
+                                        <img src="/images/work/snakebite-data/s-data-bars.png" alt="" className="w-full min-w-[14rem]" />
+                                    </div>
                                 </div>
                             </div>
-                            <p className="caption cap-top">initial</p>
+                            <div className="flex gap-[1.5rem]">
+                                <div className="flex flex-col gap-[0.4rem] w-1/3">
+                                    <p className="caption">second version: illustrations</p>
+                                    <p className="">Illustrations showed what the climates might look like, but the visuals were arbitrarily-sized and not very informative</p>
+                                </div>
+                                <div className="s-data-bg work-add-img-cont subtle-shadow w-2/3">
+                                    <div className="self-center m-[1rem]">
+                                        <img src="/images/work/snakebite-data/s-data-sun.png" alt="" className="w-full min-w-[14rem]" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-row gap-[2rem]">
+                                <div className="flex flex-col gap-[0.4rem] w-1/2">
+                                    <p className="caption">Final version: isotypes</p>
+                                    <p className="">Isotypes being representative of actual proportions of the numbers makes it much easier to tell the values of each measurement and just how much the differences are.</p>
+                                </div>
+                                <div className="s-data-bg work-add-img-cont subtle-shadow fit-content">
+                                    <img src="/images/work/snakebite-data/s-data-circles.png" alt="" className="self-center h-[10rem] m-[1rem]" />
+                                </div>
+                            </div>
                         </div>
-                        <div>
+                    </div> */}
+
+
+                    {/* <div>
+                        <div className="s-data-bg work-add-img-cont">
+                            <div className="self-center m-[1rem]">
+                                <img src="/images/work/snakebite-data/isotype1.png" alt="" className="w-full min-w-[18rem]" />
+                            </div>
+                        </div>
+                        <p className="caption cap-top">final</p>
+                    </div> */}
+
+
+                    <div className="flex flex-col w-slim self-center gap-[3rem]">
+                        <div className="flex flex-col gap-[0.6rem] left-border">
+                            <div className="flex flex-col gap-[0.3rem]">
+                                <p className="caption">first version: bars</p>
+                                <p>Arbitrary bar sizes, only takeaway is the difference in size and the actual value difference</p>
+                            </div>
+                            <div className="s-data-bg work-add-img-cont">
+                                <div className="self-center m-[1rem]">
+                                    <img src="/images/work/snakebite-data/s-data-bars.png" alt="" className="w-full min-w-[18rem]" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-[0.6rem] left-border">
+                            <div className="flex flex-col gap-[0.3rem]">
+                                <p className="caption">second version: Illustrations</p>
+                                <p>Illustrations showed what the climates might look like, but the visuals were arbitrarily-sized and not very informative</p>
+                            </div>
+                            <div className="s-data-bg work-add-img-cont">
+                                <div className="self-center m-[1rem]">
+                                    <img src="/images/work/snakebite-data/s-data-sun.png" alt="" className="w-[14vw]" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-[0.6rem] pl-[0.8rem] border-l-[8px] border-l-[#cfcfcf]">
+                            <div className="flex flex-col gap-[0.3rem]">
+                                <p className="caption"><span className="font-[20px] text-[#616161]">★</span> Final version: Isotypes</p>
+                                <p>Isotypes being representative of actual proportions of the numbers makes it much easier to tell the values of each measurement and just how much the differences are.</p>
+                            </div>
+                            <div className="s-data-bg work-add-img-cont">
+                                <div className="self-center m-[1rem]">
+                                    <img src="/images/work/snakebite-data/s-data-circles.png" alt="" className="w-[14vw]" />
+                                </div>
+                            </div>
                             <div className="s-data-bg work-add-img-cont">
                                 <div className="self-center m-[1rem]">
                                     <img src="/images/work/snakebite-data/isotype1.png" alt="" className="w-full min-w-[18rem]" />
                                 </div>
                             </div>
-                            <p className="caption cap-top">final</p>
                         </div>
                     </div>
                 </div>
