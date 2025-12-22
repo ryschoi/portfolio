0bd7410cce8804dbc367ba02ab8676de4d0976a9
ryschoi <choi.re@northeastern.edu>
2025-12-22 14:09:56 -0800

    delete old project pages

--- src/projects/radien.tsx
+++ src/projects/radien.tsx
@@ -9,10 +9,10 @@ export default function Radien() {
 
     return (
         <div className="project-content">
-            <ProjectHeader project={thisProject as Project} />
+            {/* <ProjectHeader project={thisProject as Project} /> */}
 
             <div className="project-content-rest">
-                <Task task="Conduct UX research to get a better understanding of pain points in cross-team work handoff, and design a high-fidelity prototype of an application that responds to user needs and the startup's goals for improving workflow efficiency." processItems={["Interviews", "Solution ideation", "High-fidelity prototype"]} />
+                {/* <Task task="Conduct UX research to get a better understanding of pain points in cross-team work handoff, and design a high-fidelity prototype of an application that responds to user needs and the startup's goals for improving workflow efficiency." processItems={["Interviews", "Solution ideation", "High-fidelity prototype"]} /> */}
                 <p className="mob-mid-w">Process coming soon.</p>
                 {/* <div className="col22">
                     <div className="row22">
