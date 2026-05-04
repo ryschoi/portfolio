b2d140450bf347d4dae1153e1af37bb61b1bc06e
Rebecca Choi <rebecca.choi05@gmail.com>
2026-05-03 22:34:10 -0700

    hide floating nav on mobile

--- src/components/floating-nav.tsx
+++ src/components/floating-nav.tsx
@@ -32,7 +32,7 @@ export default function FloatingNav({ sections }: { sections: section[] }) {
     }, [sections]);
 
     return (
-        <div className="floating-nav group">
+        <div className="floating-nav group hidden tablet:block">
             {sections.map((section) => (
                 <button
                     key={section.id}
